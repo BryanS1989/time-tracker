f6c0635ec82d51fb84adab050c562ccc0b6566ef
BryanS1989 <bryanstevenal@gmail.com>
2023-03-21 20:16:13 +0100

    First Commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router';
+</script>
+
+<template>
+	<header>
+		<h1>Time Tracker</h1>
+		<nav>
+			<RouterLink to="/">Registro de Tiempo</RouterLink>
+		</nav>
+	</header>
+
+	<RouterView />
+</template>
+
+<style scoped></style>
---
+++ src/views/TimeRegisterView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import TimeTracker from '../components/TimeTracker.vue';
+</script>
+
+<template>
+	<main>
+		<TimeTracker />
+	</main>
+</template>
