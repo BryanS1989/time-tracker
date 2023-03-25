9c4eb2f471cbdf9aba701bc469e5c2c6cd7a2f25
BryanS1989 <bryanstevenal@gmail.com>
2023-03-25 02:03:34 +0100

    Install Fontawesome

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,24 @@ import router from './router';
 
 import './assets/main.css';
 
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import { faPlay, faPause, faStop } from '@fortawesome/free-solid-svg-icons'
+
+/* add icons to the library */
+library.add(faPlay, faPause, faStop);
+
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+
+/* add font awesome icon component */
+app.component('font-awesome-icon', FontAwesomeIcon);
 
 app.mount('#app');
