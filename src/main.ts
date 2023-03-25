6a01ca192d3937a980223be3628368422bdb8651
BryanS1989 <bryanstevenal@gmail.com>
2023-03-25 15:18:35 +0100

    Add faAngleDown icon

--- src/main.ts
+++ src/main.ts
@@ -13,10 +13,10 @@ import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
 /* import specific icons */
-import { faPlay, faPause, faStop } from '@fortawesome/free-solid-svg-icons';
+import { faPlay, faPause, faStop, faAngleDown } from '@fortawesome/free-solid-svg-icons';
 
 /* add icons to the library */
-library.add(faPlay, faPause, faStop);
+library.add(faPlay, faPause, faStop, faAngleDown);
 
 const app = createApp(App);
 
